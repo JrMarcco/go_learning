a5a29790b1a816c2d361898eb85d0346bace9fa0
jrmarcco <jrmarcco@gmail.com>
2022-05-30 16:56:13 +0800

    RecursiveMutex

--- concurrence/mutex/RecursiveMutex.go
+++ concurrence/mutex/RecursiveMutex.go
@@ -2,35 +2,30 @@ package mutex
 
 import (
 	"fmt"
-	"runtime"
-	"strconv"
-	"strings"
 	"sync"
 	"sync/atomic"
 )
 
 type RecursiveMutex struct {
 	sync.Mutex
-	owner     int64 // 持有锁的goroutineId
-	recursion int32 // 重入次数
+	owner     int64
+	recursion int32
 }
 
-func (rm *RecursiveMutex) Lock() {
-	gid := GoId()
-	if atomic.LoadInt64(&rm.owner) == gid {
+func (rm *RecursiveMutex) Lock(token int64) {
+	if atomic.LoadInt64(&rm.owner) == token {
 		rm.recursion++
 		return
 	}
 
 	rm.Mutex.Lock()
-	atomic.StoreInt64(&rm.owner, gid)
+	atomic.StoreInt64(&rm.owner, token)
 	rm.recursion = 1
 }
 
-func (rm *RecursiveMutex) UnLock() {
-	gid := GoId()
-	if atomic.LoadInt64(&rm.owner) != gid {
-		panic(fmt.Sprintf("wrong the owner(%d): %d!", rm.owner, gid))
+func (rm *RecursiveMutex) UnLock(token int64) {
+	if atomic.LoadInt64(&rm.owner) != token {
+		panic(fmt.Sprintf("wrong the owner(%d): %d!", rm.owner, token))
 	}
 	rm.recursion--
 	if rm.recursion != 0 {
@@ -38,15 +33,4 @@ func (rm *RecursiveMutex) UnLock() {
 	}
 	atomic.StoreInt64(&rm.owner, -1)
 	rm.Mutex.Unlock()
-}
-
-func GoId() int64 {
-	var buf [64]byte
-	n := runtime.Stack(buf[:], false)
-	idField := strings.Fields(strings.TrimPrefix(string(buf[:n]), "goroutine "))[0]
-	id, err := strconv.Atoi(idField)
-	if err != nil {
-		panic(fmt.Sprintf("cannot get goroutine id: %v", err))
-	}
-	return int64(id)
 }
