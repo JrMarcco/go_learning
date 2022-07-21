e4623e64a39603de7ab64ed022987ef048a5439c
jrmarcco <jrmarcco@gmail.com>
2022-07-21 18:38:49 +0800

    commit msg

--- simple_web/framework/trie.go
+++ simple_web/framework/trie.go
@@ -30,6 +30,7 @@ func NewTrieTree() *TrieTree {
 func (tree *TrieTree) AddRouter(url string, handler ControllerHandler) error {
 	url = strings.TrimLeft(url, "/")
 
+	// 判断是否路径已经注册
 	rootNode := tree.root
 	if rootNode.findMatchNode(url) != nil {
 		return errors.New("router exist: " + url)
@@ -42,6 +43,7 @@ func (tree *TrieTree) AddRouter(url string, handler ControllerHandler) error {
 
 		children := rootNode.filterChildren(segment)
 		if len(children) > 0 {
+			// 按照分段查找子节点
 			for _, child := range children {
 				if child.segment == segment {
 					objNode = child
@@ -51,15 +53,18 @@ func (tree *TrieTree) AddRouter(url string, handler ControllerHandler) error {
 		}
 
 		if objNode == nil {
+			// 子节点不存在则构建新的子节点
 			child := newNode()
 			child.segment = segment
 			if isLast {
+				// 当前为叶子
 				child.isLeafNode = true
 				child.handler = handler
 			}
 			rootNode.children = append(rootNode.children, child)
 			objNode = child
 		}
+		// 以子节点为根继续构建子树
 		rootNode = objNode
 	}
 	return nil
@@ -73,17 +78,20 @@ func (tree *TrieTree) FindHandler(url string) ControllerHandler {
 	return nil
 }
 
+// filterChildren 过滤符合条件的备选节点
 func (node *trieTreeNode) filterChildren(segment string) []*trieTreeNode {
 	if len(node.children) == 0 {
 		return nil
 	}
 
+	// 当前节点有通配符则其子节点则为备选节点
 	if isWildSegment(segment) {
 		return node.children
 	}
 
 	nodes := make([]*trieTreeNode, 0, len(node.children))
 	for _, child := range node.children {
+		// 节点有通配符节点或节点全文匹配
 		if isWildSegment(child.segment) || child.segment == segment {
 			nodes = append(nodes, child)
 		}
@@ -92,17 +100,24 @@ func (node *trieTreeNode) filterChildren(segment string) []*trieTreeNode {
 	return nodes
 }
 
+// findMatchNode 查找匹配节点
 func (node *trieTreeNode) findMatchNode(url string) *trieTreeNode {
 	url = strings.TrimLeft(url, "/")
+
+	// 按 "/" 分割为两段
+	// 分两段是为了分级依次递归查找匹配的 node
 	segments := strings.SplitN(url, "/", 2)
 
+	// 查找第一层匹配节点
 	children := node.filterChildren(segments[0])
 	if children == nil || len(children) == 0 {
 		return nil
 	}
 
+	// 当前路径剩下最后一段
 	if len(segments) == 1 {
 		for _, child := range children {
+			// 匹配的节点必须为叶子节点
 			if child.isLeafNode {
 				return child
 			}
@@ -111,6 +126,7 @@ func (node *trieTreeNode) findMatchNode(url string) *trieTreeNode {
 	}
 
 	for _, child := range children {
+		// 将剩余段交给子节点递归查找
 		matchNode := child.findMatchNode(segments[1])
 		if matchNode != nil {
 			return matchNode
@@ -119,6 +135,7 @@ func (node *trieTreeNode) findMatchNode(url string) *trieTreeNode {
 	return nil
 }
 
+// isWildSegment 判断是否为通配 segment
 func isWildSegment(segment string) bool {
 	return strings.HasPrefix(segment, ":")
 }
